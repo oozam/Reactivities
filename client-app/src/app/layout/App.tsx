d275672cddb967decf70d883df3afbb5a412e924
oozam <dawidek199710@wp.pl>
2020-03-07 16:53:40 +0100

    Added template for activities details modal page

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import axios from 'axios';
+import { Header, Icon, List, Container } from 'semantic-ui-react'
+import { IActivity } from '../../models/activity';
+import { NavBar } from '../../features/nav/NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<IActivity[]>('http://localhost:5000/api/activities')
+      .then((response)=> {
+        setActivities(response.data)
+      });
+  }, [])
+
+    return (<>
+         <NavBar />
+         <Container style={{marginTop: '7em'}}>
+           <ActivityDashboard activities={activities} />
+         </Container>
+      </>)
+}
+
+export default App;
